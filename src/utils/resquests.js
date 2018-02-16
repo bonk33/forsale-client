5595484864bead84dcfc558d8cce34e9af0d2d77
Daniel Addae <nanaayimda@gmail.com>
2018-02-16 18:12:54 +0000

    adding checks to cookies and csrf

--- src/utils/resquests.js
+++ src/utils/resquests.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BASEURL } from './baseRequestEndpoints';
 
 const cookies = document.cookie ? document.cookie.split('; ') : "";
-const CSRFTOKEN = cookies.indexOf('csrftoken') >= 0? cookies.find((item) => item.match(/^csrftoken/)).slice("csrftoken".length + 1) : "none";
+const CSRFTOKEN = cookies.indexOf('csrftoken') >= 0 ? cookies.find((item) => item.match(/^csrftoken/)).slice("csrftoken".length + 1) : "none";
 
 export const requestWithCSRF = axios.create({
   baseURL: BASEURL,

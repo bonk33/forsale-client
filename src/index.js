bbb55b2ee1537272a3a6c405035272ff6d62e5da
Daniel Addae <nanaayimda@gmail.com>
2018-02-16 19:26:17 +0000

    removed service worker

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import registerServiceWorker from './registerServiceWorker';
-
 const client = new ApolloClient({
     link: new HttpLink({ uri: 'https://bonk33.pythonanywhere.com/graphql' }),
     cache: new InMemoryCache()
@@ -19,4 +17,3 @@ ReactDOM.render(
         <App />
     </ApolloProvider>, 
     document.getElementById('root'));
-registerServiceWorker();

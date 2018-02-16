41a9b218561de11f5ae3409a2d2d611ce386f145
Daniel Addae <nanaayimda@gmail.com>
2018-02-16 18:05:14 +0000

    made cookie check for it existence or set to empty string

--- src/utils/resquests.js
+++ src/utils/resquests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { BASEURL } from './baseRequestEndpoints';
 
-const cookies = document.cookie.split('; ');
+const cookies = document.cookie ? document.cookie.split('; ') : "";
 const CSRFTOKEN = cookies.find((item) => item.match(/^csrftoken/)).slice("csrftoken".length + 1)
 
 export const requestWithCSRF = axios.create({
